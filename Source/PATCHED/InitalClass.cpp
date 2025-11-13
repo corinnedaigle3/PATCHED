7c02e068c78c4acefe4d8e2464678bda7dbc2bc0
popoyo07 <piwedaa@gmail.com>
2025-11-13 12:55:16 -0500

    adding c++

---
+++ Source/PATCHED/InitalClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InitalClass.h"
+
+InitalClass::InitalClass()
+{
+}
+
+InitalClass::~InitalClass()
+{
+}
