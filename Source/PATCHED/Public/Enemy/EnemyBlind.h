031733ec51876343c1690e735a662333e1a355cd
popoyo07 <piwedaa@gmail.com>
2025-11-13 13:43:47 -0500

    Testinf doing enemies in C++

---
+++ Source/PATCHED/Public/Enemy/EnemyBlind.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Enemy.h"
+#include "EnemyBlind.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATCHED_API AEnemyBlind : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
